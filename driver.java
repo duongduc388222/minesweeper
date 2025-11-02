99588e1aa31557260ed1160273a9d668c9ebe4c5
Hassan17846 <smh17846@gmail.com>
2025-11-02 14:45:26 -0600

    basic implemnttaion of the driver class

--- driver.java
+++ driver.java
@@ -1,3 +1,51 @@
-public class Driver {
-    
+public class driver {
+
+    public static void main(String[] args) {
+        System.out.println("MINESWEEPER TEST DRIVER\n");
+        
+        testCase1();
+        
+        
+    }
+
+    public static void testCase1() {
+        System.out.println("\n A 3*3 board with 2 Mines\n");
+
+        int[][] arr1 = {
+            {0,1,-1},
+            {1,2,1},
+            {1,-1,1}
+        };
+
+        Board board = new Board(arr1);
+        System.out.println("Displaying the board");
+
+        System.out.println("\nOpening (2, 2):");
+        board.checkCells(2, 2);
+        board.displayBoard();
+        
+        System.out.println("\nOpening (0, 2) - hits mine:");
+        boolean hit = board.checkCells(0, 2);
+        System.out.println("Hit mine: " + hit);
+        board.displayFullBoard();
+    }
 }
+
+    
+
+
+    
+
+    
+
+
+
+        
+
+    
+
+    
+    
+    
+
+
